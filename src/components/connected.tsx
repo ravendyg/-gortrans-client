4ecd02bd3e180bd1fa34b6ed1be6668ce74419f5
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-10-15 12:03:45 +0700

    Create Connected React.Component

--- src/components/connected.tsx
+++ src/components/connected.tsx
@@ -2,10 +2,33 @@ import * as React from 'react';
 import { Store as IStore } from 'redux';
 import { IReduxState } from '../types/state';
 
-  /**
+// is there a better way to create two similar classes extending different ones?
+
+function _componentWillMount(
+  self: React.Component, store: IStore<IReduxState>, mapState: (newState: IReduxState) => any
+) {
+  if (store) {
+    (self as any)._unsubscribe = store.subscribe(() => {
+      _subscribeCb(self, store, mapState);
+    });
+    const newState = mapState(store.getState());
+    self.setState(newState);
+  }
+}
+
+function _subscribeCb(self: React.Component, store: IStore<IReduxState>, mapState: (newState: IReduxState) => any) {
+  const
+    globalState: IReduxState = store.getState(),
+    st = mapState(globalState)
+  ;
+
+  self.setState(st);
+}
+
+/**
  * override mapState
  * in `componentWillUnmount` call super.componentWillUnmount()
- * * in `componentWillMount` call super.componentWillMount()
+ * in `componentWillMount` call super.componentWillMount()
  */
 export abstract class Connected<IProps, IState> extends React.PureComponent<IProps, IState> {
 
@@ -19,27 +42,47 @@ export abstract class Connected<IProps, IState> extends React.PureComponent<IPro
   }
 
   componentWillMount() {
-    this._store = (this.props as any).store;
-    if (this._store) {
-      this._unsubscribe = this._store.subscribe(this.subscribeCb);
-      const newState = this.mapState && this.mapState(this._store.getState());
-      this.setState(newState);
-    }
+    _componentWillMount(this, (this.props as any).store, this.mapState);
   }
 
   componentWillUnmount() {
-    if (this._unsubscribe) {
-      this._unsubscribe();
-    }
+    this._unsubscribe();
   }
 
   private subscribeCb(): void {
-    const
-      globalState: IReduxState = this._store.getState(),
-      st: IState = this.mapState(globalState)
-      ;
+    _subscribeCb(this, this._store, this.mapState);
+  }
 
-    this.setState(st);
+  abstract mapState(newState: IReduxState): IState;
+
+}
+
+/**
+ * override mapState
+ * in `componentWillUnmount` call super.componentWillUnmount()
+ * in `componentWillMount` call super.componentWillMount()
+ */
+export abstract class ConnectedNotPure<IProps, IState> extends React.Component<IProps, IState> {
+
+  private _unsubscribe: () => void;
+  private _store: IStore<IReduxState>;
+
+  constructor() {
+    super();
+    this.subscribeCb = this.subscribeCb.bind(this);
+    this.mapState = this.mapState.bind(this);
+  }
+
+  componentWillMount() {
+    _componentWillMount(this, (this.props as any).store, this.mapState);
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  private subscribeCb(): void {
+    _subscribeCb(this, this._store, this.mapState);
   }
 
   abstract mapState(newState: IReduxState): IState;
