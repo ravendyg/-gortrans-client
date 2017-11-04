a615433a3c86c34d1cf436159b53a0468ce7ab41
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-11-04 07:20:41 +0700

    Move bus-search tests to the module directory

--- tests/index.ts
+++ tests/index.ts
@@ -4,5 +4,5 @@ import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-15';
 configure({ adapter: new Adapter.default() });
 
-require('tests/bus-search');
+require('src/modules/bus-search/tests');
 require('tests/main');
